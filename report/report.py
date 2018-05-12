8ac99552e0a1e865de1a8e8dd095e6f486756cf3
Rinat Abdullin <rinat.abdullin@gmail.com>
2018-05-12 16:27:15 +0200

    report: join charts, include cluster information

--- report/report.py
+++ report/report.py
@@ -17,13 +17,35 @@ def parse_args():
 args = parse_args()
 
 
-source = Path(args.source)
+folder = Path(args.source)
+
+meta = folder.joinpath("meta.json")
 
 
-with open(source.with_suffix('.json')) as f:
-    data = json.load(f)
 
-print(data)
+with open(folder.joinpath("meta.json")) as f:
+    meta = json.load(f)
+
+with open(folder.joinpath(meta["status_file"])) as f:
+    status = json.load(f)
+
+
+
+status_engine = status["cluster"]["configuration"]["storage_engine"]
+status_redundancy = status["cluster"]["configuration"]["redundancy"]["factor"]
+status_processes = len(status["cluster"]["processes"])
+
+meta_vm = meta["cluster"]["fdb_type"]
+meta_count = meta["cluster"]["fdb_count"]
+meta_tester = meta["cluster"]["tester_type"]
+
+experiment = "Event Store layer (go): es-append benchmark"
+setup =  """FoundationDB: {0}x {1}, {2} {3}; tester: 1x {4} - 1000 actors.
+event size: 200b, writes: 100%, streams: uniform [0..100000]""".format(meta_count, meta_vm, status_engine, status_redundancy, meta_tester)
+
+
+
+tsv_file = folder.joinpath(meta["main_tsv"])
 
 
 
@@ -38,32 +60,38 @@ mpl.rcParams["axes.formatter.useoffset"] = False
 plt.ticklabel_format(style='plain', axis='y')
 
 
-df = pd.read_table(args.source)
+df = pd.read_table(tsv_file)
 # skip first row
 df = df.drop(df.index[0])
 
 
 
-fig, ax = plt.subplots(1)
-fig.suptitle("Thoughput")
-ax.set_ylabel("Hz")
-ax.set_xlabel("Total transactions")
+fig, axs = plt.subplots(2, sharex = True)
+ax=axs[0]
+fig.suptitle(experiment)
+fig.subplots_adjust(top=0.85)
+title = ax.set_title(setup)
+ax.set_ylabel("Transactions per sec")
+#ax.set_xlabel("Total transactions")
 
 ax.plot(df["TxTotal"],df["Hz"])
 ax.set_ylim(bottom=0)
-fig.savefig("throughput.png")
-plt.close(fig)
-print("Saved throughput.png")
+# fig.savefig("throughput.png")
+#plt.close(fig)
+#print("Saved throughput.png")
 
 
 
 
-fig, ax = plt.subplots(1, sharex = True)
-fig.suptitle("Latency over time")
+#fig, ax = plt.subplots(1, sharex = True)
+ax=axs[1]
+# fig.suptitle("Latency: " + experiment)
+
+# plt.title(setup)
 ax.set_ylabel("Latency ms")
 ax.set_xlabel("Transactions performed")
 
-percentiles = ["P50", "P90", "P99", "P999", "100"]
+percentiles = ["P50", "P90", "P99", "P999"]
 
 p_count = len(percentiles)
 cmap = ListedColormap(sns.color_palette("YlGnBu_r", len(percentiles)))
@@ -82,7 +110,7 @@ ax.legend(loc='upper left', ncol=1, fancybox=True,framealpha=0.5, facecolor="whi
 
 
 
-fig.savefig("latency.png")
+fig.savefig("summary.png")
 plt.close(fig)
 
-print("Saved latency.png")
+print("Saved summary.png")
