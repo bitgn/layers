26b5a6e7abf866fe9c883f114f386135aaea158e
Rinat Abdullin <rinat.abdullin@gmail.com>
2018-05-22 17:12:14 +0200

    go/bench: tweaking build process

---
+++ go/benchmark/bin/remote.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+REMOTE=vault
+PROJ=github.com/bitgn/layers
+DEST=/tmp
+
+git push
+
+ssh $REMOTE PROJ=$PROJ DEST=$DEST 'bash -s' <<'ENDSSH'
+	set -e
+	cd $GOPATH/src/$PROJ
+	git pull
+	make build
+	mv benchcli $DEST
+ENDSSH
+
+scp $REMOTE:$DEST/benchcli .
