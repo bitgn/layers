18bc0da0ae0f30f5c5407f44593b89d1d74993fa
Rinat Abdullin <rinat.abdullin@gmail.com>
2018-05-22 17:44:25 +0200

    go/bench: deploy bench to the target directly

--- go/benchmark/bin/remote.sh
+++ go/benchmark/bin/remote.sh
@@ -5,14 +5,26 @@ REMOTE=vault
 PROJ=github.com/bitgn/layers
 DEST=/tmp
 
+
+if [ ! -f ~/.bitgn_tester ]
+then
+    echo "Need target machine"
+    exit 1
+fi
+
+TARGET=$(cat ~/.bitgn_tester)
+
 git push
 
-ssh $REMOTE PROJ=$PROJ DEST=$DEST 'bash -s' <<'ENDSSH'
-	set -e
-	cd $GOPATH/src/$PROJ/go/benchmark
-	git pull
-	make build
-	mv benchcli $DEST
+ssh $REMOTE PROJ=$PROJ TARGET=$TARGET 'bash -s' <<'ENDSSH'
+    set -e
+    cd $GOPATH/src/$PROJ/go/benchmark
+    git pull
+    make build
+    echo Copy to $TARGET
+    scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no  benchcli $TARGET:
 ENDSSH
 
-scp $REMOTE:$DEST/benchcli .
+
+
+
