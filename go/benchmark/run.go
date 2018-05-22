2adff3e296ce24bacd19c60c280c0ee2e14277c5
Rinat Abdullin <rinat.abdullin@gmail.com>
2018-05-22 23:24:35 +0200

    go/bench: run adaptive load if hz<0

--- go/benchmark/run.go
+++ go/benchmark/run.go
@@ -13,13 +13,45 @@ var (
 )
 
 func runBenchmark(ms chan metrics, hz int, l bench.Launcher) {
+	if hz > 0 {
+		runFixedThroughput(ms, hz, l)
+	} else {
+		runAdaptiveThroughput(ms, -hz, l)
+	}
+}
 
-	period := time.Duration(float64(time.Second) / float64(hz))
+func runAdaptiveThroughput(ms chan metrics, concurrency int, l bench.Launcher) {
+	for i := 0; i < concurrency; i++ {
+		xor := NewXorShift()
+
+		go func() {
+			for {
+				begin := time.Now()
+				x := xor.Next()
+
+				atomic.AddInt64(&pendingRequests, 1)
+
+				err := l.Exec(x)
+				total := time.Since(begin)
+
+				result := metrics{
+					error:       err != nil,
+					nanoseconds: total.Nanoseconds(),
+				}
+				ms <- result
+				atomic.AddInt64(&pendingRequests, -1)
+			}
+		}()
+	}
+}
+
+func runFixedThroughput(ms chan metrics, hz int, l bench.Launcher) {
 
 	var sent int
-
 	xor := NewXorShift()
 	started := time.Now()
+
+	period := time.Duration(float64(time.Second) / float64(hz))
 
 	for range time.Tick(period) {
 		begin := time.Now()
